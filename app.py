e912ec26606262f5a19c745868b70ab0dd76f65d
Sahil Bista <sahil@Sahils-MacBook-Air.local>
2025-07-12 19:14:50 +0545

    Feature/Added image extraction instead of just name for better ui

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
 movies = pickle.load(open('Movies.pkl','rb'))
 movies_list = movies['original_title'].values
 
 def fetch_poster(movie_id):
-    requests
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=189971e45eeda581e867f886edc30007'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies['original_title']==movie].index[0]
@@ -14,9 +17,12 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)),reverse=True, key=lambda x:x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_poster = []
     for i in movies_list:
+        movie_id = movies.iloc[i[0]].id
         recommended_movies.append(movies.iloc[i[0]].original_title)
-    return recommended_movies
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
 
 
 similarity = pickle.load(open('Similarity.pkl','rb'))
@@ -30,8 +36,26 @@ selected_movie_name = st.selectbox(
 )
 
 if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+    names,posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+
 
 
